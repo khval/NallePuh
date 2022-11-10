0c7f128b2952e256ac3aa28e6d97ff88945ddf2f
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2022-11-10 21:57:06 +0100

    more ugly code cleanup.

--- PUH.c
+++ PUH.c
@@ -207,12 +207,12 @@ struct PUHData*AllocPUH( void )
 		}
 		else
 		{
-			pd->m_Active							= FALSE;
+			pd->m_Active = FALSE;
 
-			pd->m_AudioMode						= AHI_INVALID_ID;
+			pd->m_AudioMode = AHI_INVALID_ID;
 
-			pd->m_SoundFunc.h_Entry		= (ULONG(*)(void)) PUHSoundFunc;
-			pd->m_SoundFunc.h_Data		= pd;
+			pd->m_SoundFunc.h_Entry = (ULONG(*)(void)) PUHSoundFunc;
+			pd->m_SoundFunc.h_Data = pd;
 
 			if ( gfxbase != NULL &&
 				( gfxbase->DisplayFlags & REALLY_PAL ) == 0 )
@@ -226,18 +226,18 @@ struct PUHData*AllocPUH( void )
 				pd->m_ChipFreq = 3546895;
 			}
 
-			pd->m_DMACON							= DMAF_MASTER;
-			pd->m_INTENA							= INTF_INTEN;
+			pd->m_DMACON = DMAF_MASTER;
+			pd->m_INTENA = INTF_INTEN;
 
-			pd->m_Intercepted					= (void*) 0xdff000;
-			pd->m_CustomDirect				= (void*) 0xdff000;
-			pd->m_CustomSize					= 0x200;
+			pd->m_Intercepted = (void*) 0xdff000;
+			pd->m_CustomDirect = (void*) 0xdff000;
+			pd->m_CustomSize = 0x200;
 
 			pd->m_SoftInt.is_Node.ln_Type = NT_EXTINTERRUPT;
-			pd->m_SoftInt.is_Node.ln_Pri	= 32;
+			pd->m_SoftInt.is_Node.ln_Pri = 32;
 			pd->m_SoftInt.is_Node.ln_Name = "NallePUH Level 4 emulation";
-			pd->m_SoftInt.is_Data				= pd;
-			pd->m_SoftInt.is_Code				= (void(*)(void)) PUHSoftInt;
+			pd->m_SoftInt.is_Data = pd;
+			pd->m_SoftInt.is_Code = (void(*)(void)) PUHSoftInt;
 		}
 	}
 
