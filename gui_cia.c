4b8a45f203d1abfa9af4c19a5acc7b5e642f71b7
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2023-10-22 13:30:08 +0200

    Immediate Joystick buttons.

--- gui_cia.c
+++ gui_cia.c
@@ -5,15 +5,15 @@ PAGE_Add,  VGroupObject,
 
 		LAYOUT_AddChild, HGroupObject, 
 			GFrame("Port1: Joystick 2 / Mouse"),
-			LAYOUT_AddChild, MakeButton(GAD_JOY1_BUTTON1),
-			LAYOUT_AddChild, MakeButton(GAD_JOY1_BUTTON2),
+			LAYOUT_AddChild, MakeButtonImmediate(GAD_JOY1_BUTTON1),
+			LAYOUT_AddChild, MakeButtonImmediate(GAD_JOY1_BUTTON2),
 
 		LayoutEnd,
 
 		LAYOUT_AddChild, HGroupObject, 
 			GFrame("Port2: Joystick 1"),
-			LAYOUT_AddChild, MakeButton(GAD_JOY2_BUTTON1),
-			LAYOUT_AddChild, MakeButton(GAD_JOY2_BUTTON2),
+			LAYOUT_AddChild, MakeButtonImmediate(GAD_JOY2_BUTTON1),
+			LAYOUT_AddChild, MakeButtonImmediate(GAD_JOY2_BUTTON2),
 
 		LayoutEnd,
 
