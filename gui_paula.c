2cb0f6e6536acdf8fffc8d58a95c63b6193476a8
Kjetil Hvalstrand <Kjetil.hvalstrand@gmail.com>
2023-10-27 18:11:36 +0200

    Created GFrame for Paula, to make it look uniform.

--- gui_paula.c
+++ gui_paula.c
@@ -2,14 +2,15 @@
 PAGE_Add,  VGroupObject,
 
 	LAYOUT_AddChild, VGroupObject,
+		GFrame("Audio options"),
 
-	LAYOUT_AddChild, MakeString(GAD_MODE_ID),
-	CHILD_Label, MakeLabel(GAD_MODE_ID),
+		LAYOUT_AddChild, MakeString(GAD_MODE_ID),
+		CHILD_Label, MakeLabel(GAD_MODE_ID),
 
-	LAYOUT_AddChild, MakeString(GAD_MODE_INFO),
-	CHILD_Label, MakeLabel(GAD_MODE_INFO),
+		LAYOUT_AddChild, MakeString(GAD_MODE_INFO),
+		CHILD_Label, MakeLabel(GAD_MODE_INFO),
 
-	LAYOUT_AddChild, MakeButton(GAD_SELECT_MODE),
+		LAYOUT_AddChild, MakeButton(GAD_SELECT_MODE),
 
 	LayoutEnd,
 	CHILD_WeightedHeight, 0,
